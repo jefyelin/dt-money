6bb06ee317e87ac0b9fcc62ce5779279c3770478
Jean Arruda <je.carlosarrudaa@gmail.com>
2021-03-20 01:52:53 -0300

    :sparkles: TransactionsTable

---
+++ src/components/TransactionsTable/style.ts
@@ -0,0 +1,43 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  margin-top: 4rem;
+`
+
+export const Table = styled.table`
+  width: 100%;
+  border-spacing: 0 0.5rem;
+`
+
+export const THead = styled.thead`
+  tr { 
+    th { 
+      color: var(--blue);
+      font-weight: 600;   
+      padding: 1.25rem 32px;
+      line-height: 1.5rem;
+      text-align: left;
+    }
+  }
+`
+
+export const TBody = styled.tbody`
+  tr {
+    background-color: var(--primary-shape);
+    border-radius: 0.3125rem;
+    
+    td {
+      color: var(--white);
+      padding: 1.25rem 32px;
+      margin-bottom: 8px;
+      
+      &.deposit {
+        color: var(--green);
+      }
+
+      &.withdraw {
+        color: var(--red);
+      }
+    }
+  }
+`
