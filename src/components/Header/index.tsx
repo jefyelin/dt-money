55ba41a3a3d1506ae612fe132fbc7770c648b7a6
Jean Arruda <je.carlosarrudaa@gmail.com>
2021-03-21 03:51:30 -0300

    :sparkles: Context Transactions

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,19 @@
 import { Button, Container, Content, Logo } from "./style";
 import logo from '../../assets/logo.svg';
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
   return (
     <Container>
       <Content>
         <Logo src={logo} alt="" />
-        <Button>Nova transação</Button>
+        <Button onClick={onOpenNewTransactionModal}>
+          Nova transação
+        </Button>
+
       </Content>
     </Container>
   )
--- src/components/Sumary/index.tsx
+++ src/components/Sumary/index.tsx
@@ -1,31 +1,63 @@
-import { Card, Container } from "./style";
-import incomeIcon from '../../assets/income.svg'
-import outcomeIcon from '../../assets/outcome.svg'
+import { Card, Container, IncomeIcon, OutcomeIcon } from "./style";
 import totalIcon from '../../assets/total.svg'
+import { useTransactions } from "../../hooks/useTransactions";
 
 export function Sumary() {
+  const { transactions } = useTransactions()
+
+  const summary = transactions.reduce((acc, transaction) => {
+    if (transaction.type === 'deposit') {
+      acc.deposits += transaction.amount
+      acc.total += transaction.amount
+    } else {
+      acc.withdraw += transaction.amount
+      acc.total -= transaction.amount
+    }
+
+    return acc;
+  }, {
+    deposits: 0,
+    withdraw: 0,
+    total: 0
+  })
+
   return (
     <Container>
       <Card>
         <div>
           <p>Entradas</p>
-          <img src={incomeIcon} alt="" />
+          <IncomeIcon />
         </div>
-        <strong>R$1000,00</strong>
+        <strong>
+          {Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.deposits)}
+        </strong>
       </Card>
       <Card>
         <div>
           <p>Saídas</p>
-          <img src={outcomeIcon} alt="" />
+          <OutcomeIcon />
         </div>
-        <strong>R$500,00</strong>
+        <strong>
+          {Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(-summary.withdraw)}
+        </strong>
       </Card>
       <Card>
         <div>
           <p>Total</p>
           <img src={totalIcon} alt="" />
         </div>
-        <strong>R$1500,00</strong>
+        <strong>
+          {Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.total)}
+        </strong>
       </Card>
     </Container>
   )
